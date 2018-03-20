1c8ddbed9add1086334d9493aff5e474370d35db
Dhaval Purohit <dhaval.purohit08@gmail.com>
2018-03-20 17:44:57 +0530

    feature(Route): adds routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Header from './Header'
+import { Switch, Route } from 'react-router-dom'
 import LinkList from './LinkList'
 import CreateLink from './CreateLink'
 import '../styles/App.css'
@@ -6,9 +8,14 @@ import '../styles/App.css'
 class App extends Component {
   render() {
     return (
-      <div>
-        <LinkList />
-        <CreateLink />
+      <div className="center w85">
+        <Header />
+        <div className="ph3 pv1 background-gray">
+          <Switch>
+            <Route exact path="/" component={LinkList} />
+            <Route exact path="/create" component={CreateLink} />
+          </Switch>
+        </div>
       </div>      
     )
   }
